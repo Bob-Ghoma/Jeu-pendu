89c9df6ca20862af40bdc1b66f532952d3b5a4ac
Hoc <b.ghoma@gmail.com>
2020-07-06 16:31:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [mot , setMot] = ["FRANCE","MESSI","MUSIQUE","ANTICONSTITUTIONNELLEMENT","Fortnite"]
+  let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  const [randomWords] =useState(mot[Math.floor(Math.random() * mot.length)])
+  const [counter, setCounter] = useState(10)
+    const [triedLetters, setTriedLetters] = useState([])
+
+  // function editMot(event) {
+  //   setMot(event.target.value)
+  // }
+  function displayLife() {
+      return <p> Vie : {counter}</p>
+  }
+
+  function displayWords() {
+      const regex = new RegExp(triedLetters.join('|'),'')
+      let lettre = randomWords.replace(regex,'*')
+
+      return lettre
+  }
+
+   function testlettre(event) {
+       let lettre = event.target.value
+       let i = triedLetters.indexOf(lettre)
+       triedLetters.splice(i,1)
+       setTriedLetters([triedLetters])
+   }
+
+  function alphaLettre() {
+   return alphabet.map((lettre, id) => <button onClick={testlettre} key={id} value={lettre}>{lettre}</button>)
+  }
+
+  function endGame() {
+      if (counter == 0){
+          return "Pendu ! Réessayez !"
+      }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*{<input type="passwd" value={mot} onChange={editMot}/>}*/}
+      {/*<button onClick={life}>-1</button>{counter}*/}
+        <div>{displayLife()}</div>
+        <div>{displayWords()}</div>
+        <div>{alphaLettre()}</div>
+      {endGame()}
     </div>
   );
 }
-
 export default App;
